8215f26c828e88f645abc840755c79bb2354d5f2
gkrctn <gkr.cetin@gmail.com>
2022-03-13 23:26:06 +0300

    Temanın Eklenmesi ve Default Kısmı Tamamlandı

--- Core_Proje/Controllers/DefaultController.cs
+++ Core_Proje/Controllers/DefaultController.cs
@@ -12,5 +12,13 @@ namespace Core_Proje.Controllers
         {
             return View();
         }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView();
+        }
     }
 }
